aee5f8615a770868583257c6d12827ce080b231c
zakky taufiqurrohman <taufiqurrohmanzq@gmail.com>
2019-09-14 12:35:09 +0700

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 // import React from 'react';
-import React,{Component} from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  // state cannot be use inside a function componen
-  // state object must be used inside a class componen
-  // a functional uses prop.xxxx
-  // and a class uses this.state.xxx
-  state ={
-    datas:[
-      {name:'zakky',age:21},
-      {name:'andi',age:22},
-      {name:'budis',age:25}
-    ]
+const App =props => {
+
+  const [data,setdata]= useState({
+      datas:[
+        {name:'zakky',age:21},
+        {name:'andi',age:22},
+        {name:'budis',age:25}
+      ],
+      // other: 'ini other yang di replace oleh data baru jika other tdk di definisikan'
+  });
+  // karena sifatnya mereplace all variable di datas maka gunakan code ini
+  const [otherdata,setOtherData]=useState('ini data other tidak ke replace data baru');
+ 
+  console.log(data,otherdata);
+  const swicthNameHandler = () => {
+      setdata( {
+        datas:[
+          {name:'nama ini setelah di rubah',age:21},
+          {name:'change name',age:22},
+          {name:'budis',age:25}
+        ]
+      }
+      )
   }
-  swicthNameHandler = () => {
-    console.log('ok bos q');
-  }
-  render(){
+  
   return (
     <div className="App">
       {/* props */}
@@ -27,12 +36,12 @@ class App extends Component {
       <Person name="andi" age="22" > my hobby adalah:reading</Person>
       <Person name="budi" age="23"/> */}
 
-      <button onClick={this.swicthNameHandler}>click me</button>
+      <button onClick={swicthNameHandler}>click me</button>
       {/* state */}
-      <Person name={this.state.datas[0].name} age={this.state.datas[0].age}> my hobbi is<span className="warna" > coding</span></Person>
-      <Person name={this.state.datas[1].name} age={this.state.datas[1].name} > my hobby adalah:reading</Person>
-      <Person name={this.state.datas[2].name} age={this.state.datas[2].name}/>
+      <Person name={data.datas[0].name} age={data.datas[0].age}> my hobbi is<span className="warna" > coding</span></Person>
+      <Person name={data.datas[1].name} age={data.datas[1].name} > my hobby adalah:reading</Person>
+      <Person name={data.datas[2].name} age={data.otherState} />
     </div>
   )};
-}
+
 export default App;
